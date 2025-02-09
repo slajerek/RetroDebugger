170184c0862e2160f3dd7b0b5a908017c07bda7f
Marcin Skoczylas <slajerek@gmail.com>
2025-02-09 13:48:45 +0100

    removed sdl2 from build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -3,13 +3,8 @@
 
 mkdir -p build
 cd build
-mkdir -p sdl2
 mkdir -p MTEngineSDL
 mkdir -p c64d
-
-cd sdl2
-cmake ../../MTEngineSDL/other/lib/SDL2-2.0.10-static/
-make -j$(nproc) SDL2-static
 
 cd ../MTEngineSDL
 cmake ../../MTEngineSDL/
